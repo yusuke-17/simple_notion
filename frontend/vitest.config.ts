bc5eecad69ba5e294a4e57478293f4c4e8891612
yusuke-17 <yuyr1711@gmail.com>
2026-01-13 10:33:59 +0900

    svelteTesingの追加

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vitest/config'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { svelteTesting } from '@testing-library/svelte/vite'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(), svelteTesting()],
   test: {
     globals: true,
     environment: 'jsdom',
